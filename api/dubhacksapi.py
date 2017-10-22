2b52aa70c8a11e797b7105d83d45aa134cf3095e
Arash Rai <arashrai17@gmail.com>
2017-10-21 23:40:15 -0700

    adding api

---
+++ api/dubhacksapi.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user="root",
+                     passwd="root", db="DubHacks2017")
+cur = db.cursor()
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class login(Resource):
+    def post(self):
+        content = request.get_json()
+        cur.execute("SELECT password FROM users WHERE username = \'" + content["username"] + "\';")
+        data = cur.fetchall()
+        print(data)
+        if not data:
+            return False
+        elif data[0] == content["password"]:
+            return True
+        return False
+
+
+class signup(Resource):
+    def post(self):
+        content = request.get_json()
+        print(content)
+        return content["signup"]
+
+
+api.add_resource(login, '/login')
+api.add_resource(signup, '/signup')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
