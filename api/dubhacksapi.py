c72b887893753509e8f16336cac8554f4d19daaa
Arash Rai <arashrai17@gmail.com>
2017-10-22 11:21:53 -0700

    ASD

--- api/dubhacksapi.py
+++ api/dubhacksapi.py
@@ -75,7 +75,7 @@ def actuallyjoinroom(message):
     leave_room(message['username'])
     join_room(room)
     print(room, message['username'])
-    sleep(5)
+    sleep(1)
     emit('status', {'msg': message['username'] + ' has entered the room.'}, room=room)
 
 
